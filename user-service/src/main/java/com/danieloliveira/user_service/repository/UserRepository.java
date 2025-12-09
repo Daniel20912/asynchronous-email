1739f7c1b493ef285d9aadeb7af0a10a01079d8c
Daniel Oliveira <danieloliveiradejesus2003@gmail.com>
2025-12-09 18:29:00 -0300

    feat: creation of the user repository

---
+++ user-service/src/main/java/com/danieloliveira/user_service/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.danieloliveira.user_service.repository;
+
+import com.danieloliveira.user_service.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+}
