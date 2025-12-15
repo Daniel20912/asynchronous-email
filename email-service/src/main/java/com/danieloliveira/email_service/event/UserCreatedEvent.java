b8ce8ac140f8722df5d3517dc796b6150df6bc53
Daniel Oliveira <danieloliveiradejesus2003@gmail.com>
2025-12-15 18:03:12 -0300

    fix: some errors fixed

--- email-service/src/main/java/com/danieloliveira/email_service/event/UserCreatedEvent.java
+++ email-service/src/main/java/com/danieloliveira/email_service/event/UserCreatedEvent.java
@@ -2,7 +2,9 @@ package com.danieloliveira.email_service.event;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class UserCreatedEvent {
