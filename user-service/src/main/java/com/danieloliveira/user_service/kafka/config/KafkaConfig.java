7398ccc70e0dba46e287b60daae18018ae342730
Daniel Oliveira <danieloliveiradejesus2003@gmail.com>
2025-12-15 18:16:21 -0300

    feat: creation of KafkaConfig

---
+++ user-service/src/main/java/com/danieloliveira/user_service/kafka/config/KafkaConfig.java
@@ -0,0 +1,27 @@
+package com.danieloliveira.user_service.kafka.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfig {
+
+    private static final Integer PARTITION_COUNT = 1;
+    private static final Integer REPLICA_COUNT = 1;
+
+    @Value("${spring.kafka.topic.user-created-v1}")
+    private String topicName;
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder
+                .name(topicName)
+                .partitions(PARTITION_COUNT)
+                .replicas(REPLICA_COUNT)
+                .build();
+    }
+
+}
